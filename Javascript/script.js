02a0c785d680600ae5b50c2bdc745bab90c0e6f4
Maruf Khan <marufatrai12@gmail.com>
2023-11-13 09:42:55 +0600

    JS-Class-4

---
+++ Javascript/script.js
@@ -0,0 +1,24 @@
+
+
+    //  Dynamic Initialization Variables.
+
+        var fName = "Maruf";
+        var lName = " Khan";
+
+        document.write(fName);
+        
+        document.write(lName);
+
+
+        // First  Initialization Variables.
+
+    var address;
+    var age;
+
+    address = "Rajshahi";
+
+    age = 17;
+
+    alert (address);
+
+    console.log(age);
